fd0c141e33721bd2c8e4c3d51c8063a1846d3daa
Bar Peled <42574095+Barpel@users.noreply.github.com>
2022-07-11 22:38:35 +0300

    Delete actionTypes.ts

--- src/store/actionTypes.ts
+++
@@ -1,5 +0,0 @@
-export const ADD_NOTE = "ADD_NOTE";
-export const REMOVE_NOTE = "REMOVE_NOTE";
-export const EDIT_NOTE = "EDIT_NOTE";
-export const GET_NOTES = "GET_NOTES";
-export const GET_SORTED_NOTES = "GET_SORTED_NOTES";
